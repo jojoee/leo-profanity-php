173ff3d50d5a6cf94b5c3336d2becab5dff06428
Nathachai Thongniran <inid3a@gmail.com>
2017-03-18 00:26:24 +0700

    Add all methods except clean method

--- src/LeoProfanity.php
+++ src/LeoProfanity.php
@@ -2,21 +2,217 @@
 
 namespace Jojoee\Library;
 
+use Jojoee\Library\Util;
+
 /**
  * Class LeoProfanity
  * @package Jojoee\Library
  */
 class LeoProfanity
 {
+    /** @var string[] */
     private $words = [];
+
+    /** @var array */
     private $wordDictionary = [];
+
+    /** @var Util */
+    private $util = null;
 
     /**
      * LeoProfanity constructor.
+     *
+     * @todo improve method for importing asset file
      */
     public function __construct()
     {
+        $this->util = new Util();
         $this->wordDictionary['default'] = include __DIR__ . '/dictionary/default.php';
         $this->words = $this->wordDictionary['default'];
+    }
+
+    /**
+     * Remove word from the list
+     *
+     * @param string $str
+     */
+    private function removeWord($str)
+    {
+        $key = array_search($str, $this->words);
+        if ($key !== false) {
+            unset($this->words[$key]);
+        }
+    }
+
+    /**
+     * Add word into the list
+     *
+     * @param string $str
+     */
+    private function addWord($str)
+    {
+        $key = array_search($str, $this->words);
+        if ($key === false) {
+            $this->words[] = $str;
+        }
+    }
+
+    /**
+     * Return replacement word from key
+     *
+     * @example
+     * getReplacementWord('*', 3)
+     * return '***'
+     *
+     * @example
+     * getReplacementWord('-', 4)
+     * return '----'
+     *
+     * @param string $key
+     * @param integer $n
+     * @return string
+     */
+    private function getReplacementWord($key, $n)
+    {
+        $replacementWord = '';
+        for ($i = 0; $i < $n; $i++) {
+            $replacementWord .= $key;
+        }
+
+        return $replacementWord;
+    }
+
+    /**
+     * Get word dictionary
+     * Now, we only have default dictionary
+     *
+     * @param string $dictionaryName
+     * @return string[]
+     */
+    private function getDictionary($dictionaryName)
+    {
+        $result = [];
+        if (array_key_exists($dictionaryName, $this->wordDictionary)) {
+            $result = $this->wordDictionary[$dictionaryName];
+        }
+
+        return $result;
+    }
+
+    /**
+     * Return all profanity words
+     *
+     * @return string[]
+     */
+    public function getList()
+    {
+        return $this->words;
+    }
+
+    /**
+     * Sanitize string for this project
+     * 1. Convert to lower case
+     * 2. Replace comma and dot with space
+     *
+     * @param string $str
+     * @return string
+     */
+    private function sanitize($str)
+    {
+        $str = strtolower($str);
+        $str = preg_replace('/[,.]/', ' ', $str);
+
+        return $str;
+    }
+
+    /**
+     * Check the string contain profanity words or not
+     * Approach, to make it fast ASAP
+     *
+     * @param string $str
+     * @return boolean
+     */
+    public function check($str)
+    {
+        $isFound = false;
+        if (!$str) return false;
+
+        $str = $this->sanitize($str);
+        $strs = explode(' ', $str);
+
+        foreach ($this->words as $word) {
+            if (in_array($word, $strs)) {
+                $isFound = true;
+                break;
+            }
+        }
+
+        return $isFound;
+    }
+
+    /**
+     * Add word to the list
+     *
+     * @param string|string[] $data
+     */
+    public function add($data)
+    {
+        switch (gettype($data)) {
+            case 'string':
+                $this->addWord($data);
+                break;
+            case 'array':
+                foreach ($data as $word) {
+                    $this->addWord($word);
+                }
+                break;
+            default:
+                break;
+        }
+
+        return $this;
+    }
+
+    /**
+     * Remove word from the list
+     *
+     * @param string|string[] $data
+     */
+    public function remove($data)
+    {
+        switch (gettype($data)) {
+            case 'string':
+                $this->removeWord($data);
+                break;
+            case 'array':
+                foreach ($data as $word) {
+                    $this->removeWord($word);
+                }
+                break;
+            default:
+                break;
+        }
+
+        return $this;
+    }
+
+    /**
+     * Reset word list by using default dictionary (also remove word that manually add)
+     */
+    public function reset()
+    {
+        $this->words = $this->getDictionary('default');
+
+        return $this;
+    }
+
+    /**
+     * Clear word list
+     */
+    public function clearList()
+    {
+        $this->words = [];
+
+        return $this;
     }
 }
--- tests/LeoProfanityTest.php
+++ tests/LeoProfanityTest.php
@@ -1,16 +1,167 @@
 <?php
 
-namespace Jojoee\Library\LeoProfanity;
+namespace Jojoee\Library\Tests;
 
+use Jojoee\Library\LeoProfanity;
 use PHPUnit\Framework\TestCase;
 
 class LeoProfanityTest extends TestCase
 {
     /**
-     * Test that true does in fact equal true
+     * Test getList
      */
-    public function testTrueIsTrue()
+    public function testGetIndicesOf()
     {
-        $this->assertTrue(true);
+        $filter = new LeoProfanity();
+
+        $this->assertContains('boob', $filter->getList());
+    }
+
+    /**
+     * Test check
+     */
+    public function testCheck()
+    {
+        $filter = new LeoProfanity();
+
+        // no bad word
+        $this->assertFalse($filter->check('I have 2 eyes'));
+
+        // normal case
+        $this->assertTrue($filter->check('2g1c'));
+
+        // first & last
+        $this->assertTrue($filter->check('zoophilia'));
+        $this->assertTrue($filter->check('lorem 2g1c ipsum'));
+        $this->assertTrue($filter->check('lorem zoophilia ipsum'));
+        $this->assertTrue($filter->check('I have boob, etc.'));
+    }
+
+    /**
+     * Test check by empty string
+     */
+    public function testCheckEmptyString()
+    {
+        $filter = new LeoProfanity();
+
+        $this->assertFalse($filter->check(''));
+    }
+
+    /**
+     * Test check with case sensitive
+     */
+    public function testCheckWithCaseSensitive()
+    {
+        $filter = new LeoProfanity();
+
+        $this->assertTrue($filter->check('I have BoOb'));
+    }
+
+    /**
+     * Test check with comma and dot
+     */
+    public function testCheckWithCommaAndDot()
+    {
+        $filter = new LeoProfanity();
+
+        $this->assertTrue($filter->check('I have BoOb,'));
+        $this->assertTrue($filter->check('I have BoOb.'));
+    }
+
+    /**
+     * Test check multi-occurrence
+     */
+    public function testCheckMultiOccurrence()
+    {
+        $filter = new LeoProfanity();
+
+        $this->assertTrue($filter->check('I have boob,boob, ass, and etc.'));
+    }
+
+    /**
+     * Test check unspaced word
+     */
+    public function testCheckUndetectedUnspacedWord()
+    {
+        $filter = new LeoProfanity();
+
+        $this->assertFalse($filter->check('Buy classic watches online'));
+    }
+
+    /**
+     * Test add by string
+     */
+    public function testAddByString()
+    {
+        $filter = new LeoProfanity();
+        $filter->add('b00b');
+
+        $this->assertContains('b00b', $filter->getList());
+    }
+
+    /**
+     * Test add by array
+     */
+    public function testAddByArray()
+    {
+        $filter = new LeoProfanity();
+        $filter->add(['b@@b', 'b##b']);
+
+        $this->assertContains('b@@b', $filter->getList());
+        $this->assertContains('b##b', $filter->getList());
+    }
+
+    /**
+     * Test add duplicate word
+     * it should not add duplicate word
+     */
+    public function testAddDuplicateWord()
+    {
+        $filter = new LeoProfanity();
+        $filter->add(['b@@b', 'b##b']);
+        $numberOfCurrentWords = count($filter->getList());
+        $filter->add(['b@@b', 'b##b']);
+
+        $this->assertEquals(count($filter->getList()), $numberOfCurrentWords);
+    }
+
+    /**
+     * Test remove by string
+     */
+    public function testRemoveByString()
+    {
+        $filter = new LeoProfanity();
+        $filter->remove('boob');
+
+        $this->assertNotContains('boob', $filter->getList());
+    }
+
+    /**
+     * Test remove by array
+     */
+    public function testRemoveByArray()
+    {
+        $filter = new LeoProfanity();
+        $filter->remove(['boob', 'boobs']);
+
+        $this->assertNotContains('boob', $filter->getList());
+        $this->assertNotContains('boobs', $filter->getList());
+    }
+
+    public function testReset()
+    {
+        $filter = new LeoProfanity();
+        $numberOfCurrentWords = count($filter->getList());
+        $filter->add(['badword1', 'badword2']);
+        $filter->reset();
+
+        $this->assertEquals(count($filter->getList()), $numberOfCurrentWords);
+    }
+
+    public function testClearList()
+    {
+        $filter = new LeoProfanity();
+        $filter->clearList();
+        $this->assertEmpty($filter->getList());
     }
 }
